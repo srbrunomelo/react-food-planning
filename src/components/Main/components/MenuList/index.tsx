65beb6fada250ccf2a991f3938adeeead30467f5
Bruno Melo <srbrunomelo@gmail.com>
2022-02-28 21:09:39 -0300

    Refatorando change relacionado ao evento de trocar tema

--- src/components/Main/components/MenuList/index.tsx
+++ src/components/Main/components/MenuList/index.tsx
@@ -9,12 +9,13 @@ export const MenuList = ({ ...rest }) => {
   const { theme, change, ThemeAction } = useContextTheme()
 
   const handleIcon = theme === ThemeAction.LIGHT ? 'Moon' : 'Sun'
+  const handleLabelTheme = theme === ThemeAction.LIGHT ? ThemeAction.DARK : ThemeAction.LIGHT
 
   return (
     <Wrapper {...rest}>
       <MenuListItem path="/" icon={'Home'} active={isCurrentPage('/')} />
       <MenuListItem path="/wishlist" icon={'Heart'} active={isCurrentPage('/wishlist')} /> 
-      <MenuListItem onClick={() => change(theme === ThemeAction.LIGHT ? ThemeAction.DARK : ThemeAction.LIGHT)} icon={handleIcon} /> 
+      <MenuListItem onClick={() => change(handleLabelTheme)} icon={handleIcon} /> 
     </Wrapper>
   )
 }
