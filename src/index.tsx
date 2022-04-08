e8bdb3284690e7c317f14a9ba2639622c85c76ec
Bruno Melo <srbrunomelo@gmail.com>
2022-04-08 13:36:08 -0300

    Instalando e configurando LIB react-gtm-module

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,14 @@ import App from './App'
 
 import GlobalContext from './context'
 
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM,
+}
+
+TagManager.initialize(tagManagerArgs)
+
 ReactDOM.render(
   <React.StrictMode>
     <GlobalContext>
